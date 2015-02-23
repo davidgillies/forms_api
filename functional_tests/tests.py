9cd985a155ac329971a8697ba43a4a2c18b9d6a0
David Gillies <david.gillies.rhu@gmail.com>
2015-02-23 11:33:24 +0000

    reqs change and initial test setup

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,1 +1,18 @@
+from selenium import webdriver
+import unittest
 
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        # waits a little to let browser respond
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.refresh()
+        self.browser.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
