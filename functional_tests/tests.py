2090e443358a823a9d7baf3e26f903fe6c2c6883
David Gillies <david.gillies.rhu@gmail.com>
2015-02-23 14:00:01 +0000

    fixed test to test for json response, added beautifulsoup4

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
 import unittest
+import json
+
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,8 +18,10 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_general_url_works(self):
         self.browser.get('http://localhost:8000/bladebla/etc/')
-        self.assertIn('bladebla', self.browser.title)
-        self.assertIn('etc', self.browser.title)
+        soup = BeautifulSoup(self.browser.page_source)
+        dict_from_soup = json.loads(soup.find('body').text)
+        self.assertIn('bladebla', dict_from_soup['bladebla'])
+        self.assertIn('etc', dict_from_soup['etc'])
 
 
 if __name__ == '__main__':
--- general_api/views.py
+++ general_api/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import View
+import json
 
 
 class Index(View):
     def get(self, request, a, b):
-        return HttpResponse('<title>%s %s</title>' % (a, b))
+        out = {}
+        out[a] = a
+        out[b] = b
+        # return HttpResponse('<title>%s %s</title>' % (a, b))
+        return HttpResponse(json.dumps(out), content_type='application/json')
